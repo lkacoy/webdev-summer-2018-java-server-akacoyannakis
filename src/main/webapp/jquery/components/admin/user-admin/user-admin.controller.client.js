cb4c926d4e8813aa417fd856b442aa632710fcab
lkacoy <lkacoyannakis@gmail.com>
2018-05-19 22:49:59 -0400

    dynamically add rows to the user admin table

--- src/main/webapp/jquery/components/admin/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/admin/user-admin/user-admin.controller.client.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+(function hello() {
+	
+	$(main); //declare listener on document to wait until load
+	
+	function main() {
+		var h1 = $('h1');
+		h1.css('color', 'red');
+		h1.html('User Administration');
+		
+		var tr = $('.template');
+		
+		var users = [
+			{username: 'bob'},
+			{username: 'charlie'}
+		]
+		
+		var tbody = $('tbody');
+		for (var i=0; i < users.length; i++) {
+			var user = users[i];
+			console.log(user);
+			var clone = tr.clone();
+			clone.find('.username').html(user.username);
+			tbody.append(clone);
+		}
+		
+	}
+
+})();
