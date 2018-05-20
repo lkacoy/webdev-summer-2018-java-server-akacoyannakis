f4c5c1a07334b37e5aad9fa7c416a4d39433d412
lkacoy <lkacoyannakis@gmail.com>
2018-05-20 11:47:39 -0400

    create post for user

--- src/main/webapp/jquery/components/admin/user-admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/admin/user-admin/user-admin.controller.client.js
@@ -8,10 +8,34 @@
 
 		tbody = $('tbody');
 		template = $('.template');
+		$('#createUser').click(createUser);
+		
 		var promise = fetch('http://localhost:8080/api/user');
 		var users = promise.then(function(response) {
 			return response.json();
 		}).then(renderUsers);
+	}
+	
+	function createUser() {
+		var username = $('#usernameFld').val();
+		var password = $('#passwordFld').val();
+		var firstName = $('#firstNameFld').val();
+		var lastName = $('#lastNameFld').val();
+		
+		var user = {
+				username: username,
+				password: password,
+				firstName: firstName,
+				lastName: lastName
+		};
+		
+		fetch('http://localhost:8080/api/user', {
+			method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+		});
 	}
 
 	function renderUsers(users) {
