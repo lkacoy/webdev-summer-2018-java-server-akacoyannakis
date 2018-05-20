4d9a09cfafde8e78ba081d0022cbc8398fc62c2b
lkacoy <lkacoyannakis@gmail.com>
2018-05-19 23:00:19 -0400

    UserRepository should be interface

--- src/main/java/com/example/webdevsummer2018alexandrakacoyannakis/repositories/UserRepository.java
+++ src/main/java/com/example/webdevsummer2018alexandrakacoyannakis/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.example.webdevsummer2018alexandrakacoyannakis.repositories;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer2018alexandrakacoyannakis.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
 
 }
