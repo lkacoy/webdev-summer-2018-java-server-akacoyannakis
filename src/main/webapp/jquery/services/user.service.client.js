7746d0855c8516525ba6b6187e3fbe7e7da58052
lkacoy <lkacoyannakis@gmail.com>
2018-05-20 17:35:00 -0400

    implement delete user

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -1,11 +1,17 @@
 function UserServiceClient() {
     this.createUser = createUser;
     this.findAllUsers = findAllUsers;
-/*    this.findUserById = findUserById;
+//    this.findUserById = findUserById;
     this.deleteUser = deleteUser;
-    this.updateUser = updateUser;*/
+   // this.updateUser = updateUser;
     this.url = 'http://localhost:8080/api/user';
     var self = this;
+    
+    function deleteUser(userId) {
+    	return fetch(self.url + '/' + userId, {
+    		method: 'delete'
+    	});
+    }
     
     function findAllUsers() {
     	return fetch(self.url)
@@ -23,9 +29,8 @@ function UserServiceClient() {
             }
 		});
     }
-/*    function findAllUsers(callback) { … }
+/* 
     function findUserById(userId, callback) { … }
-    function updateUser(userId, user, callback) { … }
-    function deleteUser(userId, callback) { … }*/
+    function updateUser(userId, user, callback) { … }*/
 }
 
