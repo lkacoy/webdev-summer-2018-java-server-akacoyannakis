acac5076b6e577c6433d034388300ce03e185079
lkacoy <lkacoyannakis@gmail.com>
2018-05-20 22:30:14 -0400

    add findByUsername to UserRepository

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -12,5 +12,9 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 	Iterable<User> findUserByCredentials(
 		@Param("username") String username, 
 		@Param("password") String password);
+	
+	@Query("SELECT u FROM User u WHERE u.username=:username")
+	Iterable<User> findUserByUsername(
+			@Param("username") String username);
 
 }
