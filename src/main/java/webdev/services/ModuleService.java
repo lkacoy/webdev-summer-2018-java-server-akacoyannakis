1ce65b4cbe481020216252233d2c99095f849c6c
lkacoy <lkacoyannakis@gmail.com>
2018-06-01 20:59:22 -0400

    add deleteModule and updateModue

--- src/main/java/webdev/services/ModuleService.java
+++ src/main/java/webdev/services/ModuleService.java
@@ -3,11 +3,15 @@ package webdev.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +51,27 @@ public class ModuleService {
 			return course.getModules();
 		}
 		return null;		
+	}
+	
+	@DeleteMapping("/api/course/{courseId}/module/{moduleId}")
+	public void deleteModule(@PathVariable("courseId") int courseId,
+						@PathVariable("moduleId") int moduleId) {
+		moduleRepository.deleteById(moduleId);
+	} 
+	
+	@PutMapping("/api/course/{courseId}/module/{moduleId}")
+	public Module  updateModule(@RequestBody Module module, HttpSession session) {
+		Module currentModule = (Module) session.getAttribute("module");
+		Optional<Module> data = moduleRepository.findById(currentModule.getId());
+		
+		if (data.isPresent()) {
+			currentModule = data.get();
+			currentModule.setId(module.getId());
+			currentModule.setTitle(module.getTitle());
+			
+			return currentModule;
+		}
+		return null;
 	}
 
 }
