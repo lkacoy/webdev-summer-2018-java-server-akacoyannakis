c8b7fa91284d48cc822cd93300e93a570dcf6149
lkacoy <lkacoyannakis@gmail.com>
2018-06-01 21:22:01 -0400

    add get lessons

--- src/main/java/webdev/services/LessonService.java
+++ src/main/java/webdev/services/LessonService.java
@@ -1,11 +1,19 @@
 package webdev.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import webdev.models.Course;
 import webdev.models.Lesson;
+import webdev.models.Module;
 import webdev.repositories.LessonRepository;
 import webdev.repositories.ModuleRepository;
 
@@ -18,9 +26,29 @@ public class LessonService {
 	@Autowired
 	LessonRepository lessonRepository;
 	
-	@PostMapping
-	public Lesson createLesson() {
-		
+	@PostMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public Lesson createLesson(@PathVariable("courseId") int courseId, 
+			@PathVariable("moduleId") int moduleId,
+			@RequestBody Lesson newLesson) {
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		if(data.isPresent()) {
+			Module module = data.get();
+			newLesson.setModule(module);
+			return lessonRepository.save(newLesson);
+		}
+		return null;
+
 	}
-	
+
+	@GetMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public List<Lesson> findAllLessonsForModule(
+			@PathVariable("courseId") int courseId,
+			@PathVariable("moduleId") int moduleId) {
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		if(data.isPresent()) {
+			Module module = data.get();
+			return module.getLessons();
+		}
+		return null;		
+	}
 }
