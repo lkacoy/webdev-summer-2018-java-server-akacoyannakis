689d9728b1f943c661e2fec334406b010947cad6
lkacoy <lkacoyannakis@gmail.com>
2018-06-01 20:58:21 -0400

    Revert "add updateCourse"
    This reverts commit 7a41922c8279bb554eeca55fe071357c8b221377.

--- src/main/java/webdev/services/CourseServices.java
+++ src/main/java/webdev/services/CourseServices.java
@@ -1,16 +1,11 @@
 package webdev.services;
 
-import java.util.Optional;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,24 +34,6 @@ public class CourseServices {
 	public void deleteCourse(@PathVariable("courseId") int id) {
 		courseRepository.deleteById(id);
 	}
-	
-	@PutMapping("/api/course")
-	public Course updateCourse(@RequestBody Course course, HttpSession session) {
-		Course currentCourse = (Course) session.getAttribute("course");
-		Optional<Course> data = courseRepository.findById(currentCourse.getId());
-		
-		if (data.isPresent()) {
-			currentCourse = data.get();
-			currentCourse.setId(course.getId());
-			currentCourse.setCreated(course.getCreated());
-			currentCourse.setModified(course.getModified());
-			currentCourse.setModules(course.getModules());
-			currentCourse.setTitle(course.getTitle());
-			currentCourse.setOwnedBy(course.getOwnedBy());
-			
-			return currentCourse;
-		}
-		return null;
-	}
+
 
 }
