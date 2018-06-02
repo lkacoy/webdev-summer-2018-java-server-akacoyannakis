0c26229e9ce3371febb6bb2c139a2f1e9f8099fc
lkacoy <lkacoyannakis@gmail.com>
2018-06-01 21:04:09 -0400

    create base LessonService

---
+++ src/main/java/webdev/services/LessonService.java
@@ -0,0 +1,19 @@
+package webdev.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.repositories.LessonRepository;
+import webdev.repositories.ModuleRepository;
+
+@RestController
+@CrossOrigin(origins="*", maxAge=3600)
+public class LessonService {
+
+	@Autowired
+	ModuleRepository moduleRepository;
+	@Autowired
+	LessonRepository lessonRepository;
+	
+}
