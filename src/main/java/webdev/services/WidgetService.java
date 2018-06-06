1cb4a9bff821cdc0039faed13ff937f271ee08be
lkacoy <lkacoyannakis@gmail.com>
2018-06-05 23:04:31 -0400

    add get mapping

--- src/main/java/webdev/services/WidgetService.java
+++ src/main/java/webdev/services/WidgetService.java
@@ -3,6 +3,7 @@ package webdev.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import webdev.models.Widget;
@@ -14,6 +15,7 @@ public class WidgetService {
 	@Autowired
 	WidgetRepository repository;
 	
+	@GetMapping("/api/widget")
 	public List<Widget> findAllWidgets() {
 		return (List<Widget>) repository.findAll();
 	}
