a0a2869531e3a20305c3b168d50cb295a8a4796d
lkacoy <lkacoyannakis@gmail.com>
2018-06-05 23:03:15 -0400

    create WidgetService

---
+++ src/main/java/webdev/services/WidgetService.java
@@ -0,0 +1,20 @@
+package webdev.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Widget;
+import webdev.repositories.WidgetRepository;
+
+@RestController
+public class WidgetService {
+	
+	@Autowired
+	WidgetRepository repository;
+	
+	public List<Widget> findAllWidgets() {
+		return repository.findAll();
+	}
+}
