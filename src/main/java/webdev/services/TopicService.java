72be7e01e4df98326c2a3db612c1d3c7f4fb613b
lkacoy <lkacoyannakis@gmail.com>
2018-06-12 23:12:27 -0400

    findAllTopics

--- src/main/java/webdev/services/TopicService.java
+++ src/main/java/webdev/services/TopicService.java
@@ -1,5 +1,35 @@
 package webdev.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import webdev.models.Lesson;
+import webdev.models.Topic;
+import webdev.repositories.LessonRepository;
+import webdev.repositories.TopicRepository;
+
+@RestController
+@CrossOrigin(origins="*")
 public class TopicService {
 
+	@Autowired
+	LessonRepository lessonRepository;
+	@Autowired
+	TopicRepository topicRepository;
+	
+	@GetMapping("/api/lesson/{lessonId}/topic")
+	public List<Topic> findAllTopics(@PathVariable("lessonId") int lessonId) {
+		Optional<Lesson> optionalLesson = lessonRepository.findById(lessonId);
+		if(optionalLesson.isPresent()) {
+			Lesson lesson = optionalLesson.get();
+			return lesson.getTopics();
+		}
+		return null;
+	}
 }
