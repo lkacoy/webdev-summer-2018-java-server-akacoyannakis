ef8158beb08bc7e82a6ec688d85f3542cca904cf
lkacoy <lkacoyannakis@gmail.com>
2018-06-15 18:02:09 -0400

    add link model

--- src/main/java/webdev/models/Link.java
+++ src/main/java/webdev/models/Link.java
@@ -1,5 +1,18 @@
 package webdev.models;
 
-public class Link {
+import javax.persistence.Entity;
 
+@Entity
+public class Link extends Widget {
+
+	private String href;
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+	
 }
