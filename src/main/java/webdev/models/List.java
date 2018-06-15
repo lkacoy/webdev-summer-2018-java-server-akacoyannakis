de0d472680a82b4fd9815b97dbbda8bb4b29ff4a
lkacoy <lkacoyannakis@gmail.com>
2018-06-15 18:08:03 -0400

    add list model

--- src/main/java/webdev/models/List.java
+++ src/main/java/webdev/models/List.java
@@ -1,5 +1,27 @@
 package webdev.models;
 
-public class List {
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
+@Entity
+public class List extends Widget {
+
+	private String listItems;
+	@Enumerated(EnumType.STRING)
+	private ListType listType;
+	public String getListItems() {
+		return listItems;
+	}
+	public void setListItems(String listItems) {
+		this.listItems = listItems;
+	}
+	public ListType getListType() {
+		return listType;
+	}
+	public void setListType(ListType listType) {
+		this.listType = listType;
+	}
+	
+	
 }
