2b276cc4ceb7d8abaab6a84783ab370d9e97c69f
lkacoy <lkacoyannakis@gmail.com>
2018-06-15 18:04:16 -0400

    add image model

--- src/main/java/webdev/models/Image.java
+++ src/main/java/webdev/models/Image.java
@@ -1,5 +1,18 @@
 package webdev.models;
 
-public class Image {
+import javax.persistence.Entity;
 
+@Entity
+public class Image extends Widget {
+
+	private String src;
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	
 }
