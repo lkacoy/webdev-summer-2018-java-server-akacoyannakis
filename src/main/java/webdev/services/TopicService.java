43a82351d5281efc533e1a8cb9176df8a5da8365
lkacoy <lkacoyannakis@gmail.com>
2018-06-18 21:10:35 -0400

    add create new topics

--- src/main/java/webdev/services/TopicService.java
+++ src/main/java/webdev/services/TopicService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import webdev.models.Lesson;
@@ -32,4 +34,17 @@ public class TopicService {
 		}
 		return null;
 	}
+	
+	@PostMapping("/api/lesson/{lessonId}/topic") 
+	public Topic createTopic(@PathVariable("lessonId") int lessonId, 
+			@RequestBody Topic newTopic) {
+		Optional<Lesson> data = lessonRepository.findById(lessonId);
+		if(data.isPresent()) {
+			Lesson lesson = data.get();
+			newTopic.setLesson(lesson);
+			return topicRepository.save(newTopic);
+		}
+		return null;
+
+	} 
 }
