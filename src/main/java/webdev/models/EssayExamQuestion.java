d7f7c443462b40b3c7c097bc1e12c07f01baf9e7
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 18:03:26 -0400

    add getters and setters to essay

--- src/main/java/webdev/models/EssayExamQuestion.java
+++ src/main/java/webdev/models/EssayExamQuestion.java
@@ -1,10 +1,23 @@
 package webdev.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="ESSAY_QUESTION")
-public class EssayExamQuestion {
+public class EssayExamQuestion extends BaseExamQuestion {
 
+	@Column(name="ESSAY")
+	String essay;
+
+	public String getEssay() {
+		return essay;
+	}
+
+	public void setEssay(String essay) {
+		this.essay = essay;
+	}
+	
+	
 }
