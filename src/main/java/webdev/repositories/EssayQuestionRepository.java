95204b1596a6edfa0185864a365db44a5aebad29
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 18:04:20 -0400

    add essay exam repository

---
+++ src/main/java/webdev/repositories/EssayQuestionRepository.java
@@ -0,0 +1,9 @@
+package webdev.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import webdev.models.EssayExamQuestion;
+
+public interface EssayQuestionRepository extends CrudRepository<EssayExamQuestion, Integer> {
+
+}
