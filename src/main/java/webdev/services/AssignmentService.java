68a6bed31efbde8ed6ae87cdbf4a002913e52d2b
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 19:54:03 -0400

    add delete assignment

--- src/main/java/webdev/services/AssignmentService.java
+++ src/main/java/webdev/services/AssignmentService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,10 @@ public class AssignmentService {
 			return newAssignment;
 		}
 		return null;
+	}
+	
+	@DeleteMapping("/api/assignment/{aid}")
+	public void deleteAssignmentById(@PathVariable("aid") int assignmentId) {
+		assignmentRepository.deleteById(assignmentId);
 	}
 }
