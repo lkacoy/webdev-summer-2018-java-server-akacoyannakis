740ffcace0faf6692a454daaef2f07b5bcaf5ce7
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 19:47:33 -0400

    implement find all assignments

--- src/main/java/webdev/services/AssignmentService.java
+++ src/main/java/webdev/services/AssignmentService.java
@@ -2,8 +2,10 @@ package webdev.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import webdev.models.Assignment;
 import webdev.repositories.AssignmentRepository;
 import webdev.repositories.LessonRepository;
 
@@ -15,4 +17,9 @@ public class AssignmentService {
 	LessonRepository lessonRepository;
 	@Autowired
 	AssignmentRepository assignmentRepository;
+	
+	@GetMapping("/api/assignment")
+	public Iterable<Assignment> findAllAssignments() {
+		return assignmentRepository.findAll();
+	}
 }
