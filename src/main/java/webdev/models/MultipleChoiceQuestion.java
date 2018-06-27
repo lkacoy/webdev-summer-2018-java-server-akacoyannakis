711a7043b4942d828e424f2edc4a98f2bf0851f6
lkacoy <lkacoyannakis@gmail.com>
2018-06-27 17:48:07 -0400

    add column names to multiple choice

--- src/main/java/webdev/models/MultipleChoiceQuestion.java
+++ src/main/java/webdev/models/MultipleChoiceQuestion.java
@@ -1,12 +1,15 @@
 package webdev.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "MULTIPLE_CHOICE_QUESTION")
 public class MultipleChoiceQuestion extends BaseExamQuestion {
+	@Column(name="OPTIONS", nullable=false)
 	private String options;
+	@Column(name="CORRECT_OPTION", nullable=false)
 	private int correctOption;
 	public String getOptions() {
 		return options;
